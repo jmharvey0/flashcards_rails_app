2403cd895fe977d547aa013e963c9699b137d0a7
James Moody & Josh Harvey <pair+jameswmoody.jmharvey0@devbootcamp.com>
2017-07-17 10:31:17 -0500

    add deck, card models and fix migrations

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,4 +1,4 @@
 class Round < ApplicationRecord
-  belongs_to :user_id
-  belongs_to :deck_id
+  belongs_to :user
+  belongs_to :deck
 end
--- db/migrate/20170717152228_create_rounds.rb
+++ db/migrate/20170717152228_create_rounds.rb
@@ -1,8 +1,8 @@
 class CreateRounds < ActiveRecord::Migration[5.1]
   def change
     create_table :rounds do |t|
-      t.references :user_id, foreign_key: true
-      t.references :deck_id, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :deck, foreign_key: true
       t.integer :correct_first_try
       t.integer :total_guesses
 
