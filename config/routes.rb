eface5c3203171ad05d32ba70e43722a295400bb
James Moody & Josh Harvey <pair+jameswmoody.jmharvey0@devbootcamp.com>
2017-07-17 14:59:03 -0500

    Wrap up Decks feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
-  root "decks#index"
+  root to: "decks#index"
+
+  get '/decks/:id', to: 'decks#show', as: "deck"
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
