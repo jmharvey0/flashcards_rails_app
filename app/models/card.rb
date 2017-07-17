0e843364eda5b7c8391ac2c8c90fc0c3b587d10a
James Moody & Josh Harvey <pair+jameswmoody.jmharvey0@devbootcamp.com>
2017-07-17 10:53:57 -0500

    add validations for deck and card models

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,4 @@
 class Card < ApplicationRecord
   belongs_to :deck
+  validates :question, :answer, presence: true
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,4 +2,5 @@ class Deck < ApplicationRecord
   has_many :cards, dependent: :destroy
   # Should rounds stay if the the Deck is deleted
   has_many :rounds
+  validates :name, presence: true
 end
